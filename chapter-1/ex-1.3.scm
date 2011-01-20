41b31ca3227dcd95ad37499eef3f66f211cc8de6
David Chelimsky <dchelimsky@gmail.com>
2011-01-19 22:43:22 -0600

    ex 1.3

--- chapter-1/ex-1.3.scm
+++ chapter-1/ex-1.3.scm
@@ -2,7 +2,27 @@
 
 (require "../expectations.scm")
 
-; in flight - pay no attention!!!!!!!!
-(define (sum-of-squares-of-top-2 a b c) 2)
+(define (sum-of-squares-of-top-2 a b c)
+  (cond
+    ((and (<= a b) (< a c)) (+ (* b b) (* c c)))
+    ((and (< b a) (< b c)) (+ (* a a) (* c c)))
+    (else
+     (+ (* a a) (* b b)))))
 
 (expect 2 (sum-of-squares-of-top-2 1 1 1))
+
+; this next example forced the <= in the first line
+(expect 5 (sum-of-squares-of-top-2 1 1 2))
+(expect 5 (sum-of-squares-of-top-2 1 2 1))
+(expect 5 (sum-of-squares-of-top-2 2 1 1))
+
+(expect 8 (sum-of-squares-of-top-2 1 2 2))
+(expect 8 (sum-of-squares-of-top-2 2 1 2))
+(expect 8 (sum-of-squares-of-top-2 2 2 1))
+
+(expect 13 (sum-of-squares-of-top-2 1 2 3))
+(expect 13 (sum-of-squares-of-top-2 1 3 2))
+(expect 13 (sum-of-squares-of-top-2 2 1 3))
+(expect 13 (sum-of-squares-of-top-2 2 3 1))
+(expect 13 (sum-of-squares-of-top-2 3 2 1))
+(expect 13 (sum-of-squares-of-top-2 3 1 2))
