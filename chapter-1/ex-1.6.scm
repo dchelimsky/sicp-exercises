22a4390241a2b36925ea2714232b5db5494ae67c
David Chelimsky <dchelimsky@gmail.com>
2011-01-23 10:04:04 -0600

    ex 1.6

---
+++ chapter-1/ex-1.6.scm
@@ -0,0 +1,27 @@
+#lang scheme
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(sqrt-iter 3 9)
+; results in infinite loop because both the then-clause and else-clause
+; are always evaluated because the interpreter uses applicative-order.
