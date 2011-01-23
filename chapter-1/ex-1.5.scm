42b966c513ed5e7437772d3145293489b3fe084c
David Chelimsky <dchelimsky@gmail.com>
2011-01-23 07:49:56 -0600

    ex-1.5

--- chapter-1/ex-1.5.scm
+++ chapter-1/ex-1.5.scm
@@ -12,5 +12,30 @@
 ; describe what happens with the above with:
 ;  normal-order evaluation
 ;  applicative-order evaluation
+
+;;;;;;;;;;;;;;
+; Background
 ;
-; TODO
+; The p function itself results in an infinite loop.
+
+;;;;;;;;;;;;;;
+; Normal Order
+;
+; result: 0
+;
+; this
+(test 0 (p))
+; becomes this
+(if (= 0 0) 0 (p))
+; which returns 0 before (p) is ever eval'd
+
+;;;;;;;;;;;;;;
+; Applicative Order
+;
+; result: infinite loop
+;
+; this
+(test 0 (p))
+; becomes this
+(test 0 (p))
+; etc, because (p) expands to (p), which must then be expanded (to (p)), etc
